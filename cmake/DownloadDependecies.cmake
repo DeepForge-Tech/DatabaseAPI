8f5c57e04e9a7d0edd0a3c04d3bab4045cb89518
blackflame576 <pws15548@gmail.com>
2024-07-18 02:44:29 +0300

    Refactoring

--- cmake/DownloadDependecies.cmake
+++ cmake/DownloadDependecies.cmake
@@ -7,7 +7,7 @@ include(FetchContent)
 # )
 
 # FetchContent_MakeAvailable(jsoncpp)
-
+if (USE_SQLITE)
 # SQLite
 FetchContent_Declare(SQLite
   GIT_REPOSITORY https://github.com/DeepForge-Technology/SQLite.git
@@ -15,7 +15,7 @@ FetchContent_Declare(SQLite
   DOWNLOAD_EXTRACT_TIMESTAMP TRUE
 )
 FetchContent_MakeAvailable(SQLite)
-
+endif()
 # fmt
 FetchContent_Declare(fmt
   GIT_REPOSITORY https://github.com/DeepForge-Technology/fmt.git
@@ -27,3 +27,17 @@ FetchContent_Declare(fmt
 #   add_subdirectory(${fmt_SOURCE_DIR} ${fmt_BINARY_DIR} EXCLUDE_FROM_ALL)
 # endif()
 FetchContent_MakeAvailable(fmt)
+
+# libpqxx
+if (USE_POSTGRESQL)
+FetchContent_Declare(libpqxx
+  GIT_REPOSITORY https://github.com/DeepForge-Tech/libpqxx.git
+  GIT_TAG master
+)
+# if(NOT libpqxx_POPULATED)
+#   FetchContent_Populate(libpqxx)
+
+#   add_subdirectory(${libpqxx_SOURCE_DIR} ${libpqxx_BINARY_DIR} EXCLUDE_FROM_ALL)
+# endif()
+FetchContent_MakeAvailable(libpqxx)
+endif()
